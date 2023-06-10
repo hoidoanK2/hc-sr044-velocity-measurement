cf8ae1b68d118339073d13387ee8ce5cf6e53135
hoidoanK2 <113918086+hoidoanK2@users.noreply.github.com>
2023-06-10 10:03:49 +0700

    Add files via upload

--- plot_hcsr04.py
+++ plot_hcsr04.py
@@ -1,16 +1,34 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import statistics
+from scipy.signal import medfilt
 
 file = input("Nhập tên file: ")
-x = []
+
 y = []
 for line in open(f'{file}.txt', 'r'):
-    lines = [i for i in line.split()]
-    x.append(lines[0])
-    y.append(float(lines[1]))
-      
-# plt.title("")
-# plt.xlabel('')
-# plt.ylabel('')
-plt.yticks(y)
-plt.plot(x, y, c = 'b')
+    y.append(float(line))
+z = y
+median_value = statistics.median(y)
+
+for i in range(0,len(y)):
+    if abs(y[i]) > 30*abs(median_value):
+        y[i] = median_value
+    else:
+        continue
+
+
+y = np.array(y)
+y = medfilt(y, 3)
+x = np.arange(len(y))
+
+
+plt.subplot(2,1,1)
+plt.plot(x,z,'yo-')
+plt.title('input signal')
+plt.xlabel('time')
+plt.subplot(2,1,2)
+plt.plot(x,y,'yo-')
+plt.title('filtered signal')
+plt.xlabel('time')
 plt.show()
